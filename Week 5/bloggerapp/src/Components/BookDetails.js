1b6e25ca626072c02feedce6b87b3728cb35cec2
Sashank <ksashank152@gmail.com>
2026-07-15 15:31:18 +0530

    week 5 mandatory handsocompleted

---
+++ Week 5/bloggerapp/src/Components/BookDetails.js
@@ -0,0 +1,20 @@
+function BookDetails({ books }) {
+  if (!books || books.length === 0) {
+    return <p>No books are available.</p>;
+  }
+
+  return (
+    <div>
+      <h1>Book Details</h1>
+
+      {books.map((book) => (
+        <div key={book.id}>
+          <h3>{book.bname}</h3>
+          <h4>{book.price}</h4>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default BookDetails;
